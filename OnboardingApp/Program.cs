1d665f447b0f3bca78babd9c30e422b87a0c47eb
Wes Jarvis <wes.b.jarvis@gmail.com>
2018-01-29 11:23:16 -0600

    Created a user with first and last name

--- OnboardingApp/Program.cs
+++ OnboardingApp/Program.cs
@@ -10,6 +10,21 @@ namespace OnboardingApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello! Welcome to this onboarding application?");
+
+            var user = new user();
+
+            user.FirstName = AskQuestion("What is your first name?");
+            user.LastName = AskQuestion("What is your last name?");
+
+            Console.WriteLine("Hi! " + user.FirstName + user.LastName + "!");
+            Console.WriteLine("Again, welcome!");
+        }
+
+        private static string AskQuestion(string question)
+        {
+            Console.WriteLine(question);
+            return Console.ReadLine();
         }
     }
 }
